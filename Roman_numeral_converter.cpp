ee05ba74f49b6b2feb9f570e1331258544c03ed6
BRUNOX <99665856+BrunoxFakeCoder@users.noreply.github.com>
2022-04-30 17:04:08 +0200

    Add files via upload

---
+++ Roman_numeral_converter.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int num,mill,cent,dece,unid = 0;
+string msg = "Enter a number: ";
+
+int main()
+{
+    
+    cout <<  msg;
+    cin  >>  num;
+    cout << endl;
+
+    unid = num % 10; num /= 10;
+    dece = num % 10; num /= 10;
+    cent = num % 10; num /= 10;
+    mill = num % 10; num /= 10;
+    
+    switch(mill) 
+    {
+        
+        case 1: cout << "M";    break;
+        case 2: cout << "MM";   break;
+        case 3: cout << "MMM";  break;
+        
+    }
+    
+    switch(cent)
+    {
+        
+        case 1: cout << "C";      break;
+        case 2: cout << "CC";     break;
+        case 3: cout << "CCC";    break;
+        case 4: cout << "CD";     break;
+        case 5: cout << "D";      break;
+        case 6: cout << "DC";     break;
+        case 7: cout << "DCC";    break;
+        case 8: cout << "DCCC";   break;
+        case 9: cout << "CM";     break;
+        
+    }
+    
+    switch(dece)
+    {
+        
+        case 1: cout << "X";      break;
+        case 2: cout << "XX";     break;
+        case 3: cout << "XXX";    break;
+        case 4: cout << "XL";     break;
+        case 5: cout << "L";      break;
+        case 6: cout << "LX";     break;
+        case 7: cout << "LXX";    break;
+        case 8: cout << "LXXX";   break;
+        case 9: cout << "XC";     break;
+        
+    }
+    
+    switch(unid)
+    {
+        
+        case 1: cout << "I";      break;
+        case 2: cout << "II";     break;
+        case 3: cout << "III";    break;
+        case 4: cout << "IV";     break;
+        case 5: cout << "V";      break;
+        case 6: cout << "VI";     break;
+        case 7: cout << "VII";    break;
+        case 8: cout << "VIII";   break;
+        case 9: cout << "IX";     break;
+        
+    }
+    
+    return 0;
+    
+}
